db746adb361d84162c7e05a96082552691bd596d
attila4523 <attila4523@gmail.com>
2020-03-06 19:45:59 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,12 @@
+provider "aws" {
+  region                  = "${var.region}"
+  shared_credentials_file = "~/.aws/credentials"
+  profile                 = "${var.profile}"
+  max_retries             = 5
+}
+
+terraform {
+  backend "s3" {}
+}
+
 resource "null_resource" "example" {}
