9d0bce5cb80f9f2282e7332e2c0cfd88cc0732b6
attila4523 <attila4523@gmail.com>
2020-03-06 21:09:23 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  #region                   = var.region
-  #access_key               = var.access_key
-  #secret_key               = var.secret_key
+  region                   = var.region
+  access_key               = var.access_key
+  secret_key               = var.secret_key
   #shared_credentials_file = "~/.aws/credentials"
   #profile                 = var.profile
-  #max_retries              = 5
+  max_retries              = 5
 }
 
 terraform {
