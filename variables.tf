b2a6d107a41a1ee1d100ef655e38ea86b7cc027c
attila4523 <attila4523@gmail.com>
2020-03-06 20:32:26 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,6 +2,13 @@ variable "region" {
   description = "The region to deploy to the resources to"
 }
 
-variable "profile" {
-  description = "AWS profile to use"
+#variable "profile" {
+#  description = "AWS profile to use"
+#}
+variable "access_key" {
+  description = "AWS access key"
+}
+
+variable "secret_key" {
+  description = "AWS secret key"
 }
