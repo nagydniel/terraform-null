200b73ee2635be21fffdbda5ae6a3a9442a3422d
attila4523 <attila4523@gmail.com>
2020-03-06 21:11:43 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region                   = var.region
-  access_key               = var.access_key
-  secret_key               = var.secret_key
-  #shared_credentials_file = "~/.aws/credentials"
-  #profile                 = var.profile
+  #region                   = var.region
+  #access_key               = var.access_key
+  #secret_key               = var.secret_key
+  shared_credentials_file = "~/.aws/credentials"
+  profile                 = var.profile
   max_retries              = 5
 }
 
