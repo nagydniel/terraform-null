f11fdf3ae6f9702cd1ba581860da64848171ae22
Daniel Nagy <daniel.nagy@blockben.com>
2020-04-09 11:52:51 +0200

    only null resource

--- main.tf
+++ main.tf
@@ -1,21 +1,21 @@
-provider "aws" {
-  #region                   = var.region
-  #access_key               = var.access_key
-  #secret_key               = var.secret_key
-  shared_credentials_file = "~/.aws/credentials"
-  profile                 = var.profile
-  max_retries              = 5
-}
+#provider "aws" {
+#  #region                   = var.region
+#  #access_key               = var.access_key
+#  #secret_key               = var.secret_key
+#  shared_credentials_file = "~/.aws/credentials"
+#  profile                 = var.profile
+#  max_retries              = 5
+#}
 
-terraform {
-  backend "s3" {
-    bucket = var.bucket
-    key    = "tf_cloud"
-    region = var.region
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket = var.bucket
+#    key    = "tf_cloud"
+#    region = var.region
+#  }
+#}
 
-# resource "null_resource" "example" {}
+resource "null_resource" "example" {}
 
 # resource "aws_instance" "web" {
 #   ami           = "ami-0b418580298265d5c"
